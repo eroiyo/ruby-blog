53c23e09671d82343679c32ec2c0b1c0c4e1cd33
eroiyo <eroiyo@outlook.es>
2021-11-02 18:34:53 -0400

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'post/index'
+  get 'post/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
