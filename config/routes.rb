1b304d77efb2a08087a93757d78452561006f2f3
eroiyo <eroiyo@outlook.es>
2021-11-03 05:45:09 -0400

    renmae to use plural

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/show'
-  get 'post/index'
-  get 'post/show'
+  get '/', to: 'users#index'
+  get '/user/:id', to: 'users#show'
+  get '/user/:id/post', to: 'posts#index'
+  get '/user/:id/post/:post_id', to: 'posts#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
