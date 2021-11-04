12efd855a3afd741a3cbe2401a784ae769d369b9
eroiyo <eroiyo@outlook.es>
2021-11-04 11:41:12 -0400

    databasecreation

---
+++ db/migrate/20211104115809_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.text :name
+      t.binary :photo
+      t.text :bio
+      t.integer :post_counter
+
+      t.timestamps
+    end
+  end
+end
