8b8cd0ec1532014dff94c53b1dc7b533d7abcbaa
eroiyo <eroiyo@outlook.es>
2021-11-05 05:17:55 -0400

    Test done

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,10 +2,38 @@
 
 require 'rails_helper'
 
-describe 'Posts', type: :request do
+describe 'Posts GET #index', type: :request do
+  
   it 'return true if display' do
     get '/users/1/posts'
-
     expect((response)).to have_http_status(:success)
+  end
+
+  it 'should render correct template' do
+    get '/users/1/posts'
+    expect(response).to render_template(:index)
+  end
+
+  it 'should include correct placeholder text' do
+    get '/users/1/posts'
+    expect(response.body).to include('User Post List')
+  end
+end
+
+describe 'Posts GET #show', type: :request do
+  
+  it 'return true if display' do
+    get '/users/1/posts/1'
+    expect((response)).to have_http_status(:success)
+  end
+
+  it 'should render correct template' do
+    get '/users/1/posts/1'
+    expect(response).to render_template(:show)
+  end
+
+  it 'should include correct placeholder text' do
+    get '/users/1/posts/1'
+    expect(response.body).to include('Title by Author')
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,10 +2,38 @@
 
 require 'rails_helper'
 
-describe 'Users', type: :request do
+describe 'Users GET #index', type: :request do
+  
   it 'return true if display' do
-    get '/users/1/'
-
+    get '/'
     expect((response)).to have_http_status(:success)
+  end
+
+  it 'should render correct template' do
+    get '/'
+    expect(response).to render_template(:index)
+  end
+
+  it 'should include correct placeholder text' do
+    get '/'
+    expect(response.body).to include('Users List')
+  end
+end
+
+describe 'Users GET #show', type: :request do
+  
+  it 'return true if display' do
+    get '/users/1'
+    expect((response)).to have_http_status(:success)
+  end
+
+  it 'should render correct template' do
+    get '/users/1'
+    expect(response).to render_template(:show)
+  end
+
+  it 'should include correct placeholder text' do
+    get '/users/1'
+    expect(response.body).to include('User Profile')
   end
 end
