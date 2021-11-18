6f23e988afb2a739ffc1e71733c7a308aae2328d
eroiyo <eroiyo@outlook.es>
2021-11-18 19:52:43 -0400

    specs for user index and for loging, some specs oft the user show

--- spec/features/login_test_spec.rb
+++ spec/features/login_test_spec.rb
@@ -1,15 +1,58 @@
 describe "the signin process", type: :feature do
     before :each do
-      User.make(email: 'user@example.com', password: 'password')
+        @user = User.new
+        @user.name = "User_#11"
+        @user.bio = "A bio."
+        @user.posts_counter = 0
+        @user.email= "test11@example.com"
+        @user.password  = "taawktljasktlw4aaglj"
+        @user.skip_confirmation!
+        @user.save!
     end
   
-    it "signs me in" do
-      visit '/sessions/new'
-      within("#session") do
-        fill_in 'Email', with: 'user@example.com'
-        fill_in 'Password', with: 'password'
+    it "When I click the submit button after filling in the username and the password with correct data, I am redirected to the root page, " do
+      visit '/users/sign_in'
+      within("#new_user") do
+        fill_in 'user_email', with: 'test11@example.com'
+        fill_in 'user_password', with: 'taawktljasktlw4aaglj'
       end
-      click_button 'Sign in'
-      expect(page).to have_content 'Success'
+      click_button 'Log in'
+      puts(current_path)
+      User.delete(@user.id)
+      expect(current_path).to eq("/")
     end
+
+    it "The forms fields are visible" do
+        visit '/users/sign_in'
+        a = false
+        b = false
+        within("#new_user") do
+            a = fill_in 'user_email', with: 'test11@example.com'
+            b = fill_in 'user_password', with: 'taawktljasktlw4aaglj'
+        end
+        User.delete(@user.id)
+        expect(a && b).to eq(true)
+      end
+
+    it "When I click the submit button without filling in the username and the password, I get a detailed error." do
+        visit '/users/sign_in'
+        within("#new_user") do
+          fill_in 'user_email', with: ''
+          fill_in 'user_password', with: ''
+        end
+        click_button 'Log in'
+        User.delete(@user.id)
+        expect(page).to have_content 'Invalid Email or password'
+      end
+
+          it "When I click the submit button after filling in the username and the password with incorrect data, I get a detailed error." do
+        visit '/users/sign_in'
+        within("#new_user") do
+          fill_in 'user_email', with: 'notanemail'
+          fill_in 'user_password', with: 'short'
+        end
+        click_button 'Log in'
+        User.delete(@user.id)
+        expect(page).to have_content 'Invalid Email or password'
+      end
   end
