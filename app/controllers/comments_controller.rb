20c48cc872d517f81d859c99c530335cb624fafb
eroiyo <eroiyo@outlook.es>
2021-11-18 06:59:53 -0400

    fix comment dependency

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,9 @@ class CommentsController < ApplicationController
     user = current_user
     comment_id = params[:comment_id]
     comment = Comment.find(comment_id)
+    post = Post.find(comment.post_id)
     if can? :destroy, comment
+    post.comments_counter -=1
     Comment.delete_by(id: comment_id)
     end
     redirect_to(request.env['HTTP_REFERER'])
