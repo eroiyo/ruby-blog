3cb88d3eacc9a87f9ffabd5a1556cfa615f16ea0
Sylvestre <pasytchangwa@gmail.com>
2021-11-18 12:08:28 +0200

    Add delete post function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ class CommentsController < ApplicationController
     user = current_user
     comment_id = params[:comment_id]
     comment = Comment.find(comment_id)
-    if can? :edit, comment
+    if can? :destroy, comment
     Comment.delete_by(id: comment_id)
     end
     redirect_to(request.env['HTTP_REFERER'])
